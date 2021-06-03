14e9c4e28e10beec8351fa535a676b56ec33c7be
Rodrigo Lira <rodrigocliras@gmail.com>
2021-06-02 23:34:35 -0300

    fixing url

--- mvc/sgc/projeto/urls.py
+++ mvc/sgc/projeto/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    path('', views.listar, name='listar'),
-    path('exibir/<int:projeto_id>', views.exibir, name='exibir'),
+    path('', views.listar, name='listar'), #projeto/
+    path('exibir/<int:projeto_id>', views.exibir, name='exibir'), #projeto/exibir/2
 ]
